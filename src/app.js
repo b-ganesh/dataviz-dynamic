a82728e02a4b6efc8137f421ccebdf0968edad22
b-ganesh <bganesh@uchicago.edu>
2020-03-02 00:01:50 -0600

    adding in map files

--- src/app.js
+++ src/app.js
@@ -1,23 +1,47 @@
-// if the data you are going to import is small, then you can import it using es6 import
-// import MY_DATA from './app/data/example.json'
-// (I tend to think it's best to use screaming snake case for imported json)
-const domReady = require('domready');
+// const domReady = require('domready');
 
-domReady(() => {
-  // this is just one example of how to import data. there are lots of ways to do it!
-  fetch('./data/example.json')
-    .then(response => response.json())
-    .then(data => myVis(data))
-    .catch(e => {
-      console.log(e);
-    });
-});
+// domReady(() => {
+//   // this is just one example of how to import data. there are lots of ways to do it!
+//   fetch('./data/state_laws_2014.geojson')
+//     .then(response => response.json())
+//     .then(data => myVis(data))
+//     .catch(e => {
+//       console.log(e);
+//     });
+// });
 
-function myVis(data) {
-  // portrait
-  const width = 5000;
-  const height = (36 / 24) * width;
-  console.log(data, height);
-  console.log('Hi!');
-  // EXAMPLE FIRST FUNCTION
-}
+import * as d3 from 'd3'
+
+var data_source = './data/state_laws_2014.geojson'
+d3.json(
+    data_source).then(
+    function(json) {
+      const width = 1020;
+      const height = 500;
+      const margin = {
+        top: 10,
+        left: 10,
+        right: 20,
+        bottom: 20
+      };
+  
+var svg = d3.select('body').append('svg')
+      .attr('width', margin.left + width + margin.right)
+      .attr('height', margin.top + height + margin.bottom);
+
+var projection = d3.geoAlbersUsa()
+				   .translate([width/2, height/2])
+           .scale([1090]);          
+
+var path = d3.geoPath().projection(projection);
+    
+    svg.selectAll('path')
+      .data(json.features)
+      .enter()
+      .append('path')
+      .attr("fill", "#69b3a2")
+      .attr('d', path)
+  });
+
+
+
